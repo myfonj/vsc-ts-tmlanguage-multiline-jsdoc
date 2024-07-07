8c34ed9b15ba8298a6ed1b7dbacc517c29377841
Michal Čaplygin <myfonj@gmail.com>
2024-07-07 18:00:41 +0200

    Create test01.js

---
+++ test01.js
@@ -0,0 +1,26 @@
+// @ts-check
+
+/** @typedef
+    * {
+    typeof
+    * ABC [
+         number
+    * ]
+    }
+           *       Ch -
+   this
+ - still
+ * works
+*/
+// ↑ ERROR, highlighted as a comment, but processed
+
+const ABC = /** @type {const} */(["a", "b", "c"]);
+	
+/** @type {Ch}
+*/
+// ↑ ERROR, highlighted as a comment, but processed
+var x = 'd';
+
+/** @type {Ch} */
+// ↑ OK, highlighted
+var y = 'd';
